4cdf3b262002b57d7650a8f4f34aa66a4f14f753
HOME_PC\Максим <maximkirigetov@gmail.com>
2017-07-29 01:44:27 +0300

    upd pages

--- build/js/common.js
+++ build/js/common.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
 
-	$("#nav-panel").panel().enhanceWithin();
+	// open/close nav-menu
+	$(".js-menu-btn").on('click', function(e) {
+		console.log('click');
+		e.preventDefault();
+
+		$('body').toggleClass('is-push');
+	});
+
+	// close nav-menu via swipe
+	$(".page-nav").swipe( {
+        swipeLeft:function(event, direction, distance, duration, fingerCount) {
+			if ( $('body').hasClass('is-push')) {
+         		$('body').removeClass('is-push');
+	        }
+		}
+    });
 
 	$('.js-slider').slick({
 		arrows: false,
--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
 //=include lib/jquery.js
-//=include lib/jquery.mobile-1.4.5.min.js
 //=include lib/slick.min.js
-//=include common.js  
+//=include lib/jquery.touchSwipe.min.js
+//=include common.js   
