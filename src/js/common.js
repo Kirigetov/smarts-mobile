71aa0b65d487517e3057cf595df936fda2be3d2e
HOME_PC\Максим <maximkirigetov@gmail.com>
2017-08-07 17:52:54 +0300

    upd order list

--- src/js/common.js
+++ src/js/common.js
@@ -2,7 +2,6 @@ $(document).ready(function() {
 
 	// open/close nav-menu
 	$(".js-menu-btn").on('click', function(e) {
-		console.log('click');
 		e.preventDefault();
 
 		$('body').toggleClass('is-push');
@@ -32,7 +31,12 @@ $(document).ready(function() {
 		prevArrow: '.slide-arrow_left'
 	})
 
-	$('.js-tab-link').on('click touchstart', function(e) {
+
+	// for tabs sliding
+	var touchEvent = 'onclick' in window ? 'onclick' : 'ontouchstart' ;
+	touchEvent = touchEvent.replace('on','');
+
+	$('.js-tab-link').on(touchEvent, function(e) {
 		e.preventDefault();
 
 		var _this = $(this),
@@ -40,12 +44,61 @@ $(document).ready(function() {
 			siblings = p.siblings().filter('.js-tab-item'),
 			tabContent = '.js-tab-content';
 
-console.log(siblings);
 		siblings.find('.js-tab-content').slideUp();
-		// siblings.find(tabContent).removeClass('is-active');
 		p.find(tabContent).slideToggle();
 		siblings.removeClass('is-active');
 		p.toggleClass('is-active');
 	});
 
-});
+	// js-counter
+	$(function () {
+
+	    $('.js-counter').each(function ( index, element ) {
+	        var count = 1;
+	        var $component = $(element);
+	        var $output = $component.find('.counter-output');
+
+	        var updateCount = function( increment ) {
+	            count += increment;
+	            count = count < 1 ? 1 : count;
+
+	            $output.text(count);
+	        };
+
+	        $component.on('click', '[data-increment]', function ( event ) {
+	            var increment = $(event.target).data('increment');
+	            updateCount(+increment);
+	        });
+
+	        updateCount( count ); 
+	    });
+	});
+
+
+console.log($(".header").offset().top);
+
+
+	    // scroll button
+    $(".js-scroll-top").on("click", function(){
+        $('html, body').animate({
+            scrollTop: $(".header").offset().top},500);
+        return false;
+    });
+
+    var lastScrollTop = 0;
+    $(window).scroll(function(event){
+       var st = $(this).scrollTop();
+       if (st > lastScrollTop){
+            if (!$(".js-scroll-top").hasClass("is-visible")) {
+                $(".js-scroll-top").addClass("is-visible");
+            }
+
+       } else {
+        if ($(".js-scroll-top").hasClass("is-visible")) {
+            $(".js-scroll-top").removeClass("is-visible");
+        }
+       }
+       lastScrollTop = st;
+    });
+}); 
+
